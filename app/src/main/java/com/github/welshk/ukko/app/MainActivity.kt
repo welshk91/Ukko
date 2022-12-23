be951f551c9606d747ab1f9eea18e0b339c85bc6
Kevin <welshk91@gmail.com>
2022-12-23 18:27:13 -0500

    Added ability to get user's location so we can move away from hardcoding location value

--- app/src/main/java/com/github/welshk/ukko/app/MainActivity.kt
+++ app/src/main/java/com/github/welshk/ukko/app/MainActivity.kt
@@ -1,18 +1,74 @@
 package com.github.welshk.ukko.app
 
+import android.Manifest
+import android.location.Location
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.github.welshk.ukko.R
+import com.github.welshk.ukko.utils.LocationUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Main activity.
- * Doesn't do much other than hold fragments
+ * Mainly makes sure we get proper Location info
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var permissionRequest: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        createPermissionRequest()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        launchPermissionRequest()
+    }
+
+    private fun createPermissionRequest() {
+        permissionRequest = LocationUtil.permissionRequest(this, {
+            LocationUtil.getLastKnownLocation(this, fusedLocationClient) {
+                locationSuccess(it)
+            }
+        }, {
+            LocationUtil.getLastKnownLocation(this, fusedLocationClient) {
+                locationSuccess(it)
+            }
+        }) {
+            locationAccessDenied()
+        }
+    }
+
+    private fun launchPermissionRequest() {
+        permissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    fun locationSuccess(location: Location) {
+        viewModel.setLocation(location)
+    }
+
+    fun locationAccessDenied() {
+        Snackbar.make(
+            this,
+            window.decorView.rootView,
+            "No location details given. Show stock city",
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
---
+++ app/src/main/java/com/github/welshk/ukko/app/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.github.welshk.ukko.app
+
+import android.location.Location
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * Holds the location data that we can share amoung the Fragments
+ */
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    val userLocation: LiveData<Location>
+        get() = _userLocation
+    private val _userLocation = MutableLiveData<Location>()
+
+    fun getLocation(): Location? {
+        return userLocation.value
+    }
+
+    fun setLocation(location: Location) {
+        userLocation.value?.set(location)
+    }
+}
