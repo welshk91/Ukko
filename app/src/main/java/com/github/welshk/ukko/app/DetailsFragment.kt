c5219230e97595ae55c03a44e8c48daeb9d6dcb6
Kevin <welshk91@gmail.com>
2022-12-22 22:10:45 -0500

    Lint fixes

--- app/src/main/java/com/github/welshk/ukko/app/DetailsFragment.kt
+++ app/src/main/java/com/github/welshk/ukko/app/DetailsFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.welshk.ukko.R
 import com.github.welshk.ukko.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/github/welshk/ukko/networking/RetrofitClient.kt
+++ app/src/main/java/com/github/welshk/ukko/networking/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
