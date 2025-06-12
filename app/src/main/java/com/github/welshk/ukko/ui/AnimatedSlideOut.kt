30b40be46c135bbb58f5ff877499b07ed5a7295a
welshk91 <welshk91@gmail.com>
2025-06-12 06:35:17 -0400

    Added basic animated slideout view

---
+++ app/src/main/java/com/github/welshk/ukko/ui/AnimatedSlideOut.kt
@@ -0,0 +1,58 @@
+package com.github.welshk.ukko.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+
+@Composable
+fun AnimatedSlideOut(
+    modifier: Modifier = Modifier,
+    visible: Boolean,
+    enter: EnterTransition = slideInHorizontally(
+        initialOffsetX = { -it },
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = EaseOut
+        )
+    ),
+    exit: ExitTransition = slideOutHorizontally(
+        targetOffsetX = { -it },
+        animationSpec = tween(
+            durationMillis = 200,
+            easing = EaseIn
+        )
+    ),
+    popupProperties: PopupProperties = PopupProperties(focusable = true),
+    onDismissRequest: (() -> Unit)? = null,
+    content: @Composable (AnimatedVisibilityScope.() -> Unit),
+) {
+    val visibleState = remember { MutableTransitionState(false) }
+    visibleState.targetState = visible
+
+    if (visibleState.currentState || visibleState.targetState || !visibleState.isIdle) {
+        Popup(
+            onDismissRequest = onDismissRequest,
+            properties = popupProperties
+        ) {
+            AnimatedVisibility(
+                visibleState = visibleState,
+                enter = enter,
+                exit = exit,
+                modifier = modifier,
+                content = content
+            )
+        }
+    }
+}
